9bbdfa345197f6649a2f281662fd545d0e4bba39
hakanmarthidir <hakan_hidir@hotmail.com>
2023-04-10 15:14:31 +0200

    cancelafter and deserialize object

--- websocket-client/Program.cs
+++ websocket-client/Program.cs
@@ -1,27 +1,41 @@
-﻿
-using System.Net.WebSockets;
+﻿using System.Net.WebSockets;
 using System.Text;
 
 public static class Program
 {
     public static async Task Main(string[] args)
-    {        
-        using (var ws = new ClientWebSocket())
+    {
+        try
         {
-            await ws.ConnectAsync(new Uri("ws://localhost:5012/ws"), CancellationToken.None);
-            var buffer = new byte[256];
-            while (ws.State == WebSocketState.Open)
+            using (var ws = new ClientWebSocket())
             {
-                var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
-                if (result.MessageType == WebSocketMessageType.Close)
+                //cancellation with timespan
+                //var cancelSource = new CancellationTokenSource();
+                //cancelSource.CancelAfter(TimeSpan.FromSeconds(60));
+
+                await ws.ConnectAsync(new Uri("ws://localhost:5012/ws"), CancellationToken.None);
+                var buffer = new byte[256];
+                while (ws.State == WebSocketState.Open)
                 {
-                    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
-                }
-                else
-                {
-                    Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, result.Count));
+                    var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+                    if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+                    }
+                    else
+                    {
+                        //if you need any deserialize object
+                        //var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                        //var deserializedData = JsonConvert.DeserializeObject<TObject>(message);
+                        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, result.Count));
+                    }
                 }
             }
         }
+        catch (WebSocketException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
     }
 }
